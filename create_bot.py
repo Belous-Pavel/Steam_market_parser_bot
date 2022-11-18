1bde6892b691240e6fcac648f82a242f32775cf0
Belous-Pavel <pavel.belous2002@gmail.com>
2022-11-18 22:25:24 +0300

    Initial commit

---
+++ create_bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+
+
+bot = Bot(token='5801150933:AAHqxfqNX5NoNA1XhLwPSGJ-MdOm31b8aSc')
+dp = Dispatcher(bot)
---
+++ keyboards/__init__.py
@@ -0,0 +1,6 @@
+from keyboards.client_kb import kb_client
+from keyboards.client_kb_mens import kb_client_mens
+from keyboards.client_kb import kb_profile
+from keyboards.client_kb import kb_faq
+from keyboards.client_kb import kb_game
+from keyboards.client_kb import kb_marketplace
